c92bc7490b510aee553a47b5d7d153223bd32817
Igor' Smolyanov <smolyan_91@mail.ru>
2017-01-15 12:49:44 +0500

    Add API

--- GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -1,10 +1,15 @@
 package com.example.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +22,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerTextView ;
     private Button mShowAnswer;
+    private TextView mApiTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class CheatActivity extends AppCompatActivity {
         mShowAnswer = (Button) findViewById(R.id.showerAnswerButton);
 
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue){
@@ -40,8 +47,31 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator animator = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    animator.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    animator.start();
+                }else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        mApiTextView = (TextView) findViewById(R.id.api_button_version);
+        mApiTextView.setText("API level ");
+        mApiTextView.append(Build.VERSION.SDK);
+
     }
 
     @Override
