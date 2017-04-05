2e893234e8fc3c3ef16d19d2bae0e747b48b9346
Smolyan <smolyan_91@mail.ru>
2017-04-06 00:38:03 +0300

    Upd

--- QuizApp/app/src/main/java/com/example/quizapp/QuizActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -4,12 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBunk = new Question[]{
+            new Question(R.string.question_1,true),
+            new Question(R.string.question_2,true),
+            new Question(R.string.question_3,false),
+            new Question(R.string.question_4,false),
+            new Question(R.string.question_5,true),
+            new Question(R.string.question_6,false),
+            new Question(R.string.question_7,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,8 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this, R.string.incorrect_ans, Toast.LENGTH_SHORT).show();
             }
         });
-
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBunk[mCurrentIndex].getmTextResID();
+        mQuestionTextView.setText(question);
     }
 }
