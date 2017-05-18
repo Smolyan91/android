52c01455e636c96463a7e5d19aa18c21f9815d65
Smolyan <smolyan_91@mail.ru>
2017-05-18 08:34:18 +0300

    Flow CrimeFragment

--- JobCollisions/app/src/main/java/com/example/jobcollisions/controller/CrimeFragment.java
+++ JobCollisions/app/src/main/java/com/example/jobcollisions/controller/CrimeFragment.java
@@ -1,10 +1,62 @@
 package com.example.jobcollisions.controller;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.jobcollisions.R;
+import com.example.jobcollisions.model.Crime;
 
 /**
- * Created by Администратор on 13.05.2017.
+ * Fragment
+ * @author SmolyanovIS
  */
 
 public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mEditText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    /***
+     * false -нужно ли вкл заполненное предст в родителя
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_crime, container,false);
+        mEditText = (EditText) view.findViewById(R.id.crime_title);
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return view;
+    }
 }
