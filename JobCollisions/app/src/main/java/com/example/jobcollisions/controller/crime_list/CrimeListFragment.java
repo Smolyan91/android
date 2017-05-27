3f317f3b92708e0269c3919c1f14d8dfc0c5f8ac
smolyan91 <smolyan_91@mail.ru>
2017-05-27 22:02:58 +0300

    Add fist fragment: view + controller

--- JobCollisions/app/src/main/java/com/example/jobcollisions/controller/crime_list/CrimeListFragment.java
+++ JobCollisions/app/src/main/java/com/example/jobcollisions/controller/crime_list/CrimeListFragment.java
@@ -1,6 +1,15 @@
 package com.example.jobcollisions.controller.crime_list;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jobcollisions.R;
 
 /**
  * Created by igor on 27.05.17.
@@ -8,4 +17,14 @@ import android.support.v4.app.Fragment;
 
 public class CrimeListFragment extends Fragment{
 
+    private RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
