011b1a628b76b119eeccd08e82560566035fadfa
Smolyan91 <smolyan_91@mail.ru>
2017-06-01 19:59:07 +0300

    Fix

--- JobCollisions/app/src/main/java/com/example/jobcollisions/JobCollisions.java
+++ JobCollisions/app/src/main/java/com/example/jobcollisions/JobCollisions.java
@@ -3,9 +3,7 @@ package com.example.jobcollisions;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
+
 
 import com.example.jobcollisions.controller.CrimeFragment;
 import com.example.jobcollisions.controller.SingleFragmentActivity;
