f297ef7e50fd8964c4801251d6baa1f16deceb3f
Smolyan91 <smolyan_91@mail.ru>
2017-07-04 21:47:46 +0300

    Fictiv

--- JobCollisions/app/src/main/java/com/example/jobcollisions/controller/crime_list/CrimeListActivity.java
+++ JobCollisions/app/src/main/java/com/example/jobcollisions/controller/crime_list/CrimeListActivity.java
@@ -4,9 +4,6 @@ import android.support.v4.app.Fragment;
 
 import com.example.jobcollisions.controller.SingleFragmentActivity;
 
-/**
- * Created by igor on 27.05.17.
- */
 
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
